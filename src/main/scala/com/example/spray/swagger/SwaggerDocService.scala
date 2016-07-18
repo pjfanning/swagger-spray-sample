dca704a3da74665e7c80a54a6f4be11f38323b88
PJ Fanning <pj.fanning@workday.com>
2016-07-18 16:51:32 +0100

    add more swagger config items

--- src/main/scala/com/example/spray/swagger/SwaggerDocService.scala
+++ src/main/scala/com/example/spray/swagger/SwaggerDocService.scala
@@ -6,10 +6,14 @@ import com.github.swagger.spray.SwaggerHttpService
 import com.github.swagger.spray.model.`package`.Info
 import com.example.spray.add.AddService
 import com.example.spray.hello.HelloService
+import io.swagger.models.ExternalDocs
+import io.swagger.models.auth.BasicAuthDefinition
 
 class SwaggerDocService(context: ActorRefFactory) extends SwaggerHttpService {
   implicit def actorRefFactory = context
   override val apiTypes = Seq(ru.typeOf[AddService], ru.typeOf[HelloService])
   override val host = "localhost:12345"
   override val info = Info(version = "1.0")
+  override val externalDocs = Some(new ExternalDocs("Core Docs", "http://acme.com/docs"))
+  override val securitySchemeDefinitions = Map("basicAuth" -> new BasicAuthDefinition())
 }
